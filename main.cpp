5a3a4a2379417fe88f1eaa9863574f4e7ba17592
Marcos Rodríguez Aranda <marcos@MacBook-Pro-de-Marcos.local>
2020-02-18 11:35:37 +0100

    Sigmoid activation on circularity

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
-#include <opencv/cv.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
 #include <ctime>
 #include <string>
 
@@ -35,16 +37,16 @@ int main()
     double tim = 0;
     clock_t init;
 
-    namedWindow("Image", CV_WINDOW_AUTOSIZE);
-    namedWindow("Thresholding", CV_WINDOW_AUTOSIZE);
-    namedWindow("Settings", CV_WINDOW_AUTOSIZE);
+    namedWindow("Image", WINDOW_AUTOSIZE);
+    //namedWindow("Thresholding", WINDOW_AUTOSIZE);
+    namedWindow("Settings", WINDOW_AUTOSIZE);
 
 
     resizeWindow("Settings", 640,240);
 
-    moveWindow("Image" ,600,100); //
-    moveWindow("Thresholding" ,1240,100);
-    moveWindow("Settings" ,600,640);
+    moveWindow("Image" ,100,100); //
+    //moveWindow("Thresholding" ,700,100);
+    moveWindow("Settings" ,100,420);
 
     if(!webcam.isOpened())
     {
@@ -60,6 +62,7 @@ int main()
         cout << "Unable to capture frame" << endl;
         return 1;
     }
+    resize(img,img, Size(0,0), 0.5,0.5, INTER_CUBIC );
 
     Mat featu = Mat(img.size(),img.type());
     Mat canva = Mat(img.size(), img.type());
@@ -89,24 +92,22 @@ int main()
     RNG rng;
     int lvalue = 10;
     int avalue = 160;
-    int negrest = 28;
-    int posres = 14;
+    int restrictiveness_th = 51;
+    int area = 1500;
 
-
-    createTrackbar("b_THRESHOLDING", "Settings", &lvalue, 180);
-    createTrackbar("a_THRESHOLDING", "Settings", &avalue, 255);
-    createTrackbar("- restrictiveness", "Settings", &negrest, 180);
-    createTrackbar("+ restrictiveness", "Settings", &posres, 180);
+    createTrackbar("Area threshold (px)", "Settings", &area, 2000);
+    createTrackbar("Restrictiveness (%)", "Settings", &restrictiveness_th, 100);
 
     while(keypressed != ESCAPE)
     {
 
         webcam.read(img);
+        resize(img,img, Size(0,0), 0.5,0.5, INTER_CUBIC );
         //cvtColor(canva,featu,CV_BGR2GRAY);
         init = clock();
 
         GaussianBlur(img,img,Size(7,7),0,0);
-       // medianBlur(img,img,5);
+        medianBlur(img,img,5);
         cvtColor(img,canva, COLOR_BGR2Lab);
         inRange(canva, Scalar(0,avalue,lvalue),Scalar(255,255,255),featu);
         //floodFill(im_thresholded, Point(0,0), Scalar(255,255,255));
@@ -118,10 +119,10 @@ int main()
             morphologyEx( featu,featu, CV_MOP_OPEN , Mat() );
         }*/
 
-        morphologyEx( featu,featu, CV_MOP_DILATE , Mat() );
-        morphologyEx( featu,featu, CV_MOP_DILATE , Mat() );
-        morphologyEx( featu,featu, CV_MOP_DILATE , Mat() );
-        morphologyEx( featu,featu, CV_MOP_DILATE , Mat() );
+        morphologyEx( featu,featu, MORPH_DILATE , Mat() );
+        morphologyEx( featu,featu, MORPH_DILATE , Mat() );
+        morphologyEx( featu,featu, MORPH_DILATE , Mat() );
+        morphologyEx( featu,featu, MORPH_DILATE , Mat() );
 
         number_of_labels = connectedComponentsWithStats(featu, labels, stats, centroids, connectivity);
 
@@ -133,9 +134,10 @@ int main()
         {
 
             int radius = (int) ( stats.at<int>(i,3))/2;
-            int diffdiam = (2*sqrt(stats.at<int>(i,4)/PI))-2*radius;
-
-            if(stats.at<int>(i,4) > 1750 && diffdiam > (0-negrest) && diffdiam < posres)
+            int diffdiam = abs((2*sqrt(stats.at<int>(i,4)/PI))-2*radius);
+            float circ = 1 - (1/(1+exp(-0.48*(diffdiam-9)))); //sigmoid activation
+            circ*=100; //express as percentage
+            if(stats.at<int>(i,4) > 1500 && circ > restrictiveness_th)
             {
                 Point cornerTopLeft(stats.at<int>(i,0), stats.at<int>(i,1));
                 Point cornerBottomRight(stats.at<int>(i,0) + stats.at<int>(i,2), stats.at<int>(i,1) + stats.at<int>(i,3));
@@ -148,19 +150,19 @@ int main()
                 //rectangle(img,cornerTopLeft, cornerBottomRight, color, 2);
                 circle(img,center, radius, color,3);
                 circle(img,center, 1, Scalar(215,215,215),6);
-                putText(img,"X: " + to_string(center.x) + "px", Point(centroids.at<double>(i,0), centroids.at<double>(i,1)+70) , CV_FONT_HERSHEY_PLAIN,1, Scalar(0,215,0),2);
-                putText(img,"Y: " + to_string(center.y) + "px", Point(centroids.at<double>(i,0), centroids.at<double>(i,1)+86) , CV_FONT_HERSHEY_PLAIN ,1, Scalar(0,215,0),2);
-                putText(img,"Z: " + to_string(calcZeta(radius)) + "cm", Point(centroids.at<double>(i,0), centroids.at<double>(i,1)+102) , CV_FONT_HERSHEY_PLAIN ,1, Scalar(215,215,0),2);
-                putText(img,"Circ: " + to_string(diffdiam), Point(centroids.at<double>(i,0)+ 15, centroids.at<double>(i,1)) , CV_FONT_HERSHEY_PLAIN ,1, Scalar(215,215,215)),1.4;
+                putText(img,"X: " + to_string(center.x) + "px", Point(centroids.at<double>(i,0), centroids.at<double>(i,1)+70) , FONT_HERSHEY_PLAIN,1, Scalar(0,215,0),2);
+                putText(img,"Y: " + to_string(center.y) + "px", Point(centroids.at<double>(i,0), centroids.at<double>(i,1)+86) , FONT_HERSHEY_PLAIN ,1, Scalar(0,215,0),2);
+                putText(img,"Z: " + to_string(calcZeta(radius)) + "cm", Point(centroids.at<double>(i,0), centroids.at<double>(i,1)+102) , FONT_HERSHEY_PLAIN ,1, Scalar(215,215,0),2);
+                putText(img,"Circ: " + to_string(circ), Point(centroids.at<double>(i,0)+ 15, centroids.at<double>(i,1)) , FONT_HERSHEY_PLAIN ,1, Scalar(215,215,215)),2.4;
             }
         }
 
         tim = calcTime(init, clock());
 
-        putText(img,"FPS :" + to_string((int)1/tim) + " / " +to_string(tim*1000) + " ms", Point(2,20), CV_FONT_HERSHEY_PLAIN ,1, Scalar(215,215,215));
+        putText(img,"FPS :" + to_string((int)1/tim) + " / " +to_string(tim*1000) + " ms", Point(2,20), FONT_HERSHEY_PLAIN ,1, Scalar(215,215,215));
 
         imshow("Image", img);
-        imshow("Thresholding", featu);
+        //imshow("Thresholding", featu);
 
         keypressed = waitKey(1); //waiting 3ms to key
 
